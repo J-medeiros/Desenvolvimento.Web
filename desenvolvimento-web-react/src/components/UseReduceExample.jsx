3709cca435ab96ec72a38a5bbdbe67ff3778e7e3
J-medeiros <joao.v.m.s.jacinto@academico.unirv.edu.br>
2025-04-13 12:49:31 -0300

    Alterações reduce

--- desenvolvimento-web-react/src/components/UseReduceExample.jsx
+++ desenvolvimento-web-react/src/components/UseReduceExample.jsx
@@ -1,40 +1,28 @@
 import { useReducer } from "react";
 
-const initialState = { count: 0 };
+const initialState = { count: 1 };
 
 function reducer(state, action) {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "increment":
+            return { count: state.count + 1 };
+        case "decrement":
+            return { count: state.count - 1 };
+        default:
+            return state;
+    }
 }
 
 const UseReducerExample = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-  return (
-    <div className="container d-flex flex-column align-items-center mt-5">
-      <h4>Contador: {state.count}</h4>
-      <div className="d-flex gap-3 mt-3">
-        <button
-          className="btn btn-danger"
-          onClick={() => dispatch({ type: "decrement" })}
-        >
-          -
-        </button>
-        <button
-          className="btn btn-success"
-          onClick={() => dispatch({ type: "increment" })}
-        >
-          +
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <p>Contador: {state.count}</p>
+            <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+            <button onClick={() => dispatch({ type: "increment" })}>+</button>
+        </div>
+    );
 };
 
 export default UseReducerExample;
